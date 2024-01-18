53ddb5faec08e5336e6475d62049b65bce506532
Kevin Chang <kevin@bucketanalytics.com>
2024-01-18 18:50:19 +0800

    feat: add apollo-wrapper component

---
+++ todolist-for-kevinzhang/web/src/app/ApolloWrapper.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+// ref: https://www.apollographql.com/blog/using-apollo-client-with-next-js-13-releasing-an-official-library-to-support-the-app-router
+
+import { ApolloLink, HttpLink } from "@apollo/client";
+import {
+  ApolloNextAppProvider,
+  NextSSRInMemoryCache,
+  NextSSRApolloClient,
+  SSRMultipartLink,
+} from "@apollo/experimental-nextjs-app-support/ssr";
+
+function makeClient() {
+  const httpLink = new HttpLink({
+    uri: "http://localhost:3000/graphql",
+    fetchOptions: { cache: "no-store", mode: "no-cors" },
+  });
+
+  return new NextSSRApolloClient({
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === "undefined"
+        ? ApolloLink.from([
+            new SSRMultipartLink({
+              stripDefer: true,
+            }),
+            httpLink,
+          ])
+        : httpLink,
+  });
+}
+
+export function ApolloWrapper({ children }: React.PropsWithChildren) {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
